b33fddfb29e5a8626f23517b54a29a3f648ed44c
chicagocs <chicago_computers@hotmail.com>
2025-12-12 00:35:44 -0300

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1012,8 +1012,8 @@ document.addEventListener('DOMContentLoaded', () => {
             artist: 'Radio Online',
             album: 'Las 24 horas',
             artwork: [
-                { src: '/images/icon-192.png', sizes: '192x192', type: 'image/png' },
-                { src: '/images/icon-512.png', sizes: '512x512', type: 'image/png' }
+                { src: '/images/web-app-manifest-192x192.png', sizes: '192x192', type: 'image/png' },
+                { src: '/images/web-app-manifest-512x512.png', sizes: '512x512', type: 'image/png' }
             ]
         });
         
