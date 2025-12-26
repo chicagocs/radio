f8d8a33f899a0c9da81be0d2248128f26b056fd3
chicagocs <chicago_computers@hotmail.com>
2025-12-25 23:57:50 -0300

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,6 @@
 // app.js - v3.2.7
 
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/dist/umd/supabase.js';
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/dist/esm/index.js';
 
 // ==========================================================================
 // CONFIGURACIÓN DE SUPABASE (PRESENCIA)
