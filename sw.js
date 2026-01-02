02329c9e414bf760c04b4f1ddd2723de3df710c3
chicagocs <chicago_computers@hotmail.com>
2026-01-02 14:24:36 -0300

    Update cache version to v3.7.6

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// v3.7.5
-const CACHE_VERSION = 'v3.7.5';
+// v3.7.6
+const CACHE_VERSION = 'v3.7.6';
 const STATIC_CACHE = `max-static-${CACHE_VERSION}`;
 const API_CACHE = `max-api-${CACHE_VERSION}`;
 
