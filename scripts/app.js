447d804b40fdca9fdda4ce7dfed3548cf9e2523e
chicagocs <chicago_computers@hotmail.com>
2026-01-02 23:22:02 -0300

    Fix trackStartTime calculation for newTrack date

--- scripts/app.js
+++ scripts/app.js
@@ -799,7 +799,8 @@ async function updateSomaFmInfo(bypassRateLimit = false) {
                 currentTrackInfo = newTrack;
                 updateUIWithTrackInfo(newTrack);
                 resetAlbumCover();
-                trackStartTime = newTrack.date ? (newTrack.date * 1000)+1000 : Date.now();
+                // 000
+                trackStartTime = newTrack.date ? (newTrack.date * 1000)-1000 : Date.now();
                 trackDuration = 0;
                 startCountdown();
                 
