813b7dae0132e69100a8b59f8b295fde2935b4c9
chicagocs <chicago_computers@hotmail.com>
2026-01-06 21:39:08 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// v3.6.0
-const CACHE_VERSION = 'v3.6.0';
+// v3.5.0
+const CACHE_VERSION = 'v3.5.0';
 const STATIC_CACHE = `max-static-${CACHE_VERSION}`;
 const API_CACHE = `max-api-${CACHE_VERSION}`;
 
